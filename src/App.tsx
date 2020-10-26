94a4745807c2aa2e4347545252597f993e946b23
neha jain <nehajain@nehas-MacBook-Air.local>
2020-10-26 23:18:25 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import * as React from "react";
+import "./app.pcss";
+import "./app.scss";
+
+const App = () => <h1>My React and TypeScript App!</h1>;
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.tsx",
+  output: {
+    path: path.join(__dirname, "/dist"),
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: "ts-loader",
+      },
+      {
+        test: /\.[s|p]css?$/,
+        loader: ["style-loader", "css-loader", "postcss-loader", "sass-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
+};
